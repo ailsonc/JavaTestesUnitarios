e9265561ca7772a6de5cf9a1e8e12b34ba527318
ailson <ailsonlcosta@gmail.com>
2020-06-14 15:09:37 -0300

    Assert

--- src/test/java/br/ba/acosta/servicos/LocacaoServiceTest.java
+++ src/test/java/br/ba/acosta/servicos/LocacaoServiceTest.java
@@ -1,19 +1,22 @@
-package br.ce.wcaquino.servicos;
+package br.ba.acosta.servicos;
 
-import br.ce.wcaquino.entidades.Filme;
-import br.ce.wcaquino.entidades.Locacao;
-import br.ce.wcaquino.entidades.Usuario;
-import br.ce.wcaquino.utils.DataUtils;
-import junit.framework.TestCase;
+import br.ba.acosta.entidades.Filme;
+import br.ba.acosta.entidades.Locacao;
+import br.ba.acosta.entidades.Usuario;
+import br.ba.acosta.utils.DataUtils;
+import org.hamcrest.CoreMatchers;
 import org.junit.Assert;
 import org.junit.Test;
 
 import java.util.Date;
 
-public class LocacaoServiceTest extends TestCase {
+import static org.junit.Assert.assertThat;
+
+public class LocacaoServiceTest {
 
     @Test
     public void alugarFilme() {
+        //cenario
         LocacaoService service = new LocacaoService();
         Usuario usuario = new Usuario("Ailson");
         Filme filme = new Filme("Filme",2, 1.5);
@@ -21,9 +24,17 @@ public class LocacaoServiceTest extends TestCase {
         //acao
         Locacao locacao = service.alugarFilme(usuario,filme);
 
-        //verificacao
+        //verificacao - old
         Assert.assertTrue(locacao.getValor() == 1.5);
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
         Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
+
+        //verificacao - new
+        Assert.assertEquals(1.5, locacao.getValor(), 0.01);
+        Assert.assertThat(locacao.getValor(), CoreMatchers.is(1.5));
+
+        //verificacao - import static
+        assertThat(locacao.getValor(), CoreMatchers.is(1.5));
+
     }
 }
